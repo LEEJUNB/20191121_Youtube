9f173ad50faf4424a9208d1ba3a844e6b0d8ea78
junblee <topguide82@gmail.com>
2019-11-21 13:02:38 +0900

    between

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const handle= () => {
     console.log(`Listening on : http://localhost:${port}`);
 };
 
+const between = (res,req,next) => {
+    res.send("beettttttwwweeeen!!!");
+    next();
+};
+
 const handle2 = (req,res) => {
     res.send('Listeng~~');
     console.log(`Listening on : http://localhost:${port}`);
 };
+
+app.use(between);
 app.get('/',handle2);
 app.listen(port,handle);
