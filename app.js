c9d6401ca4f7031ebbfaf163c5a284995a9aa95d
junblee <topguide82@gmail.com>
2019-11-21 18:02:16 +0900

    make a routers folder

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-import express from "express"; 
+import express from "express";
+import morgan from "morgan"; // 로깅하기_모슨일을했는지 기록 어떤접속이고 어디에접속하는지알수있음
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
 const app = express();
 
-const between = (res,req,next) => {
-    res.send("beettttttwwweeeen!!!");
-    next();
-};
+app.use(cookieParser()); // 쿠키에 유저 정보를 저장한다. 왜냐? session을 다루기위해서
+app.use(bodyParser.json()); // form을 다룰수있어야한다.
+app.use(bodyParser.urlencoded()); // form을 다룰수있어야한다.
+app.use(morgan("combined"));
+app.use(helmet());
 
-const handle2 = (req,res) => {
-    res.send('Listeng~~');
-    console.log(`Listening on : http://localhost:${port}`);
-};
-
-app.use(between);
-app.get('/',handle2);
+app.use("/",globalRouter)// global Router. root("/")가 될 것. 여긴 검색,홈 
+app.use("/user", userRouter);
+app.use("/video",videoRouter);
 
 export default app;
